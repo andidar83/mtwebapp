5c8e24ead0d5544ba95ec623f019458ae577d3cf
andidar83 <92540957+andidar83@users.noreply.github.com>
2021-10-15 01:22:11 +0700

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-
+STATIC_ROOT = [
+    BASE_DIR / "staticfiles",
+   
+]
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -133,5 +137,8 @@ STATICFILES_DIRS = [
 
 
 
-MEDIA_ROOT = r'C:\dev\Project\django_projectv2\forward\EDI2'
+MEDIA_ROOT = [
+    BASE_DIR / "EDI",
+   
+]
 MEDIA_URL = '/EDI/'
